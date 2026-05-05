ba82ae622af44b0afdc2afc3c0e5ee982cdb1f3b
Millicent <milligwyn2003@gmail.com>
2026-05-05 09:46:30 -0500

    Update SensorUtility.cpp

--- sensing/TOF/SensorUtility.cpp
+++ sensing/TOF/SensorUtility.cpp
@@ -2,15 +2,13 @@
 
 static VL53L4CX_MultiRangingData_t MultiRangingData;
 
-void initSensors(VL53L4CX &s1, VL53L4CX &s2, VL53L4CX &s3, int p1, int p2, int p3)
+void initSensors(VL53L4CX &s1, VL53L4CX &s2, int p1, int p2)
 {
   pinMode(p1, OUTPUT);
   pinMode(p2, OUTPUT);
-  pinMode(p3, OUTPUT);
 
   digitalWrite(p1, LOW);
   digitalWrite(p2, LOW);
-  digitalWrite(p3, LOW);
   delay(100);
 
   digitalWrite(p1, HIGH);
@@ -24,11 +22,6 @@ void initSensors(VL53L4CX &s1, VL53L4CX &s2, VL53L4CX &s3, int p1, int p2, int p
   s2.begin();
   s2.InitSensor(0x52);
   s2.VL53L4CX_SetDeviceAddress(0x54);
-
-  digitalWrite(p3, HIGH);
-  delay(50);
-  s3.begin();
-  s3.InitSensor(0x52);
 }
 
 void processSensors(VL53L4CX &sensor, const *char sensorName)
@@ -38,17 +31,13 @@ void processSensors(VL53L4CX &sensor, const *char sensorName)
   {
     if (sensor.VL53L4CX_GetMultiRangingData(&MultiRangingData) == 0)
     {
-      for (int j = 0; j < MultiRangingData.NumberOfObjectsFound; j++)
-      {
-        if (MultiRangingData.RangeData[j].RangeStatus == 0)
-        {
-          Serial.print(sensorName);
-          Serial.print(": ");
-          Serial.print(MultiRangingData.RangeData[j].RangeMilliMeter);
-          Serial.println("mm");
-        }
-      }
       sensor.VL53L4CX_ClearInterruptandStartMeasurement();
-    }
+      if (MultiRangingData.NumberOfObjectsFound > 0 && MultiRangingData.RangeData[0].RangeStatus == 0)
+      {
+          return MultiRangingData.RangeData[0].RangeMilliMeter;
+      }
+    }    
   }
+
+  return -1;
 }
