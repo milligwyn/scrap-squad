a1a0b96977060681a7661d04f62655f79d875198
Millicent <milligwyn2003@gmail.com>
2026-05-10 14:09:10 -0500

    Update SensorUtility.h

--- sensing/TOF/SensorUtility.h
+++ sensing/TOF/SensorUtility.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 #include <vl53l4cx_class.h>
 
-void initSensors(VL53L4CX &s1, VL53L4CX &s2, VL53L4CX &s3, int p1, int p2, int p3);
+void initSensors(VL53L4CX &s1, VL53L4CX &s2, int p1, int p2);
 void processSensors(VL53L4CX &sensor, const char* sensorName);
 
 #endif
